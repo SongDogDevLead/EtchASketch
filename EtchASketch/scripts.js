5a724c0e50f9cbafeccfe24223219fc5784b486c
SongDogDevLead <176419206+SongDogDevLead@users.noreply.github.com>
2024-08-19 18:04:37 -0500

    Add functions for generateing grids and marking

---
+++ EtchASketch/scripts.js
@@ -0,0 +1,51 @@
+
+
+    const container = document.querySelector('.container')
+    const button = document.querySelector('button')
+function generateGrid(){
+    let fullGrid = (gridSide * gridSide)
+    const dim = 100/gridSide
+    
+    container.innerHTML = '';
+    
+    for (let i = 0; i < fullGrid; i++) {
+        const noteGrid = document.createElement('div');
+        noteGrid.style.margin = 0;
+        noteGrid.style.padding = 0;
+        noteGrid.style.height = dim + 'vh';
+        noteGrid.style.width = dim + 'vw';
+        noteGrid.style.backgroundColor = 'lightgray';
+        noteGrid.className = 'drawHere';
+
+        container.appendChild(noteGrid);
+    }
+    divs = document.querySelectorAll('.drawHere');
+divs.forEach(div => {
+    div.addEventListener('mouseover', function() {
+        this.style.backgroundColor = 'black';
+    });
+});
+}
+    button.addEventListener('click', gridPrompt)
+
+function gridPrompt(){
+   let input = window.prompt(`Generate new page`, `1-100`);
+    input = parseInt(input);
+
+    if(isNaN(input)|| input < 1 || input > 100){
+        gridPromptError()
+    }
+    else {gridSide = input;
+        generateGrid();
+    }
+}
+function gridPromptError(){
+   let input = window.prompt('Error: enter a number between 1-100', 'here')
+   if(isNaN(input)|| input < 1 || input > 100){
+    gridPromptError()
+
+    }
+    else {gridSide = input;
+        generateGrid();
+    }
+}

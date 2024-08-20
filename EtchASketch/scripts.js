9763f2dd3cf67e4fa3d09c3a06b0a30d2fc1571f
SongDogDevLead <176419206+SongDogDevLead@users.noreply.github.com>
2024-08-19 21:37:25 -0500

    Make input validation DRYer

--- EtchASketch/scripts.js
+++ EtchASketch/scripts.js
@@ -1,5 +1,6 @@
     const container = document.querySelector('.container')
     const button = document.querySelector('button')
+
 function generateGrid(){
     console.log('generateGrid')
     let fullGrid = (gridSide * gridSide)
@@ -19,34 +20,30 @@ function generateGrid(){
         container.appendChild(noteGrid);
     }
     divs = document.querySelectorAll('.drawHere');
-divs.forEach(div => {
+    divs.forEach(div => {
     div.addEventListener('mouseover', function() {
         this.style.backgroundColor = 'black';
     });
 });
 }
 
-function gridPrompt(){
-    console.log('gridPrompt')
-   let input = window.prompt(`Generate new page`, `1-100`);
+
+    
+
+function gridPrompt(message = `Generate new page`, field = `Enter a number between 1-100`){
+    
+    let input = window.prompt(message, field);
     input = parseInt(input);
 
     if(isNaN(input)|| input < 1 || input > 100){
-        gridPromptError()
-    }
-    else {gridSide = input;
-        generateGrid();
-    }
-}
-function gridPromptError(){
-   let input = window.prompt('Error: enter a number between 1-100', 'here')
-   if(isNaN(input)|| input < 1 || input > 100){
-    gridPromptError()
-
+        gridPrompt('Error: Must be a valid number', 'between 1-100');
     }
     else {gridSide = input;
         generateGrid();
     }
 }
 
-button.addEventListener('click', gridPrompt)
+
+button.addEventListener('click', function() {
+    gridPrompt(); 
+})
